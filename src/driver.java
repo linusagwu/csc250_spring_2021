67269a8a929f316a1d4c7b62c15057cba8d5d404
linusagwu <78839892+linusagwu@users.noreply.github.com>
2021-02-10 02:25:46 -0500

    update code 1

--- src/driver.java
+++ src/driver.java
@@ -1,4 +1,28 @@
-
-public class driver {
-
-}
+// Java program to count vowels in a string 
+public class Driver { 
+         
+    static boolean isVowel(char ch) 
+    { 
+        ch = Character.toUpperCase(ch); 
+        return (ch=='A' || ch=='E' || ch=='I' || 
+                           ch=='O' || ch=='U'); 
+    } 
+       
+    static int countVowels(String str) 
+    { 
+        int count = 0; 
+        for (int i = 0; i < str.length(); i++) 
+            if (isVowel(str.charAt(i))) // Check for vowel 
+                ++count; 
+        return count; 
+    } 
+       
+    public static void main(String args[]) 
+    { 
+        //string object 
+        String str = "Matthew"; 
+       
+        // Total numbers of Vowel 
+        System.out.println(countVowels(str)); 
+    } 
+} 
